e578f8f032c9ce5fb4435d2054e29ac3ac212a1b
hyein <kimhyein7110@gmail.com>
2021-06-04 23:35:17 +0900

    [feature/mypage] create mypage feature mvc

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const authController = require('./authController');
 const userController = require('./userController');
 const profileController = require('./profileController');
+const mypageController = require('./mypageController');
 
 module.exports = {
   authController,
   userController,
-  profileController
+  profileController,
+  mypageController
 }
