99e3eccc777cc7b984a061b0f45e144322a4d061
hyein <kimhyein7110@gmail.com>
2021-06-11 02:34:03 +0900

    [feature/mypage] pm2 error

---
+++ controllers/mypageController.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var db = require('../db');
+var conn = db.init();
+
+// 프로필 사진, 닉네임, 소개글, 포스트 수, 팔로잉 수, 팔로워 수 GET
+const info = (req, res) => {
+  
+}
--- routes/mypageRouter.js
+++ routes/mypageRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { mypageController } = require('../controllers');
+const { verifyToken } = require('../middleware/auth');
 
 
 
