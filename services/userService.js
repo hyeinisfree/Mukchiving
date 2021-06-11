83f9c05d9128a0e85ec16775580fa12546d972f2
hyein <kimhyein7110@gmail.com>
2021-06-12 01:55:03 +0900

    [feature/userPost] create service

--- services/userService.js
+++ services/userService.js
@@ -4,7 +4,16 @@ const conn = db.init();
 db.connect(conn);
 
 const userPost = function(user_id, callback){
-
+  var params = [user_id];
+  var sql = 'select * from post where user_id = ?;';
+  conn.query(sql, params, function (err, results) {
+    if(err) {
+      callback(err);
+      return;
+    }
+    callback(null, results);
+    return;
+  });
 };
 
 module.exports = {
