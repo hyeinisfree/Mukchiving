476e9f97c66b1d73d1f5ca5e375510d0eb14027a
hyein <kimhyein7110@gmail.com>
2021-06-12 03:40:07 +0900

    [feature/detailPost] done

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const { verifyToken } = require('../middleware/auth');
 
 router.post('/create', verifyToken, postController.createPost);
+router.get('/detail/:id', verifyToken, postController.detailPost);
 
 module.exports = router;
