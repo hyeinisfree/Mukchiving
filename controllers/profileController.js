d5757132abd5618b4243d460a789feff5f9215cb
hyein <kimhyein7110@gmail.com>
2021-06-12 09:15:22 +0900

    updateProfile fix

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -16,18 +16,32 @@ const getProfile = (req, res) => {
 
 const updateProfile = (req, res, next) => {
   var user_id = req.decoded.user_id;
+  
+  const profile = profileService.getProfile(user_id, async function(err, results) {
+    if(results[0]) {
+      var data = [];
+      data.push(results[0].username);
+      data.push(results[0].content);
+      data.push(results[0].profile_image);
+      data.push(user_id);
 
-  var username = req.body.username;
-    var content = req.body.content;
-    var profile_image = req.body.profile_image;
-    const checkUsername = authService.checkUsername(username, function(err, results) {
-      if(results) return res.status(400).json({success:false, message:"해당 닉네임이 이미 존재합니다."});
-      var data = [username, content, profile_image, user_id];
-      const updateProfile = profileService.updateProfile(data, function(err, results) {
+      if(req.body.username) {
+        data[0] = req.body.username;
+        const checkUsername = authService.checkUsername(req.body.username, function(err, results) {
+          if(results) return res.status(400).json({success:false, message:"해당 닉네임이 이미 존재합니다."}); 
+        });
+      }
+      if(req.body.content) data[1] = req.body.content;
+      if(req.body.profile_image) data[2] = req.body.profile_image;
+    
+      const updateProfile = await profileService.updateProfile(data, function(err, results) {
         if(results) return res.json({success:true, message:"사용자 닉네임, 소개글, 프로필 사진 수정 성공"});
         return res.json({success:false, message:"사용자 프로필 수정 실패"});
       });
-    });
+    } else {
+      return res.status(400).json({success:false, message:"해당 사용자의 프로필이 없습니다."});
+    }
+  });
 };
 
 const uploadProfileImage = (req, res, next) => {
--- services/profileService.js
+++ services/profileService.js
@@ -17,7 +17,6 @@ const getProfile = function(user_id, callback){
 }
 
 const updateProfile = function(data, callback){
-  console.log(data);
   var params = data;
   var sql = 'update profile set username = ?, content = ?, profile_image = ? where user_id = ?;';
   conn.query(sql, params, function (err, results) {
