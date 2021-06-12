41fca4c0effa635d126927de91fba1c89d20ae0e
hyein <kimhyein7110@gmail.com>
2021-06-13 01:24:46 +0900

    upload get -> post

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -6,6 +6,6 @@ const { verifyToken } = require('../middleware/auth');
 
 router.get('/get/:id', verifyToken, profileController.getProfile);
 router.patch('/update', verifyToken, profileController.updateProfile);
-router.get('/uploadImage', verifyToken, upload.single("profileImage"), profileController.uploadProfileImage);
+router.post('/uploadImage', verifyToken, upload.single("profileImage"), profileController.uploadProfileImage);
 
 module.exports = router;
