b8befcdc0df1d1a8d4d6f9b2fd0a4477b3c2e7c3
hyein <kimhyein7110@gmail.com>
2021-06-12 09:55:13 +0900

    userPost fix

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,7 +6,7 @@ var conn = db.init();
 const userPost = (req, res) => {
   var user_id = req.params.id;
   const userPost = postService.userPost(user_id, function(err, results) {
-    if(results) return res.json({success:true, message:"사용자 포스트 반환 성공", post : results});
+    if(results[0]) return res.json({success:true, message:"사용자 포스트 반환 성공", post : results});
     return res.status(400).json({success:false, message:"해당 사용자가 작성한 포스트가 없습니다."});
   });
 }
